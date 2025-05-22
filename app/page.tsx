060cee0b94a690862528d7fd219cda1d81fb6b33
usss <88474883+sukimumuu@users.noreply.github.com>
2025-05-22 16:14:39 +0700

    style : banner and section 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,83 +1,63 @@
 import Image from "next/image";
-import Link from "next/link";
-import ussImg from "@/public/images/rizqybagus.webp";
 
-
-import Navbar from "@/components/organisms/Navbar";
-import Footer from "@/components/organisms/Footer";
-import HeroCarousel from "@/components/partials/HeroCarousel";
-import ProjectCard from "@/components/atoms/ProjectCard";
-
-import { FaLaravel } from "react-icons/fa";
-import { FaUnity } from "react-icons/fa";
-import { FaHtml5 } from "react-icons/fa";
-
-import { project_laravel } from "@/data/project_laravel"
-import { project_unity } from "@/data/project_unity"
-import { project_html } from "@/data/project_html"
+// Images
+import BannerImage from "@/public/images/keyboard-foto.webp"
+import SelfImage from "@/public/images/rizqybagus.webp"
+import IndonesiaFlag from "@/public/images/indonesia.png"
+// Icons
+import { HiMiniInboxArrowDown } from "react-icons/hi2";
+import { FiExternalLink } from "react-icons/fi";
 
 export default function Home() {
-  const laravel = project_laravel.slice(0, 3);
-  const unity = project_unity.slice(0, 3);
-  const html = project_html.slice(0, 3);
-
   return (
     <>
-    <main className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <Navbar />
-      <div className="flex flex-col items-center justify-between min-h-screen px-4 sm:px-6 lg:px-8 py-8">
-          <div className="ms-auto max-[426px]:mx-auto mb-8">
-            <HeroCarousel />
+      <div className="flex flex-col">
+        <Image src={BannerImage} alt="banner image - https://www.freepik.com/author/pikisuperstar" className="w-full h-48 object-cover" />
+        <div className="w-11/12 mx-auto">
+          <div className="w-full">
+            <div className="h-36 relative mb-8">
+              <Image src={SelfImage} alt="self picture" className="rounded-full border-4 border-white w-44 h-44 mx-auto absolute -top-8 left-0" />
+              <h2 className="text-3xl font-bold absolute top-10 left-48">Rizqy Bagus Saputra</h2>
+              <p className="text-xl text-gray-500 font-medium absolute top-20 left-48">I'm a junior web developer</p>
+              <button className="btn bg-black text-white border-black absolute top-10 right-0">
+                  <a href="mailto:rizqybs.sp@gmail.com" className="flex items-center text-white text-lg font-bold"><HiMiniInboxArrowDown className="me-3"/> Hire Me</a>
+              </button>
+            </div>
+            <h2 className="text-xl font-semibold">Experience</h2>
+            <p className="mb-3 text-gray-500">I had experience in backend web application development and API integration, convert designs from Figma to responsive view, and teamwork.</p>
+            <hr />
+            <div className="flex mt-8">
+              <div className="w-3/5">
+                <h2 className="text-xl font-semibold">About Me</h2>
+                <p className="mb-3 text-gray-500 text-justify">Hi, I'm Rizqy Bagus Saputra, a tech enthusiast with a deep passion for programming and innovation. I graduated from SMK N 1 Purwokerto, majoring in Software Engineering, and I'm currently pursuing a degree in Information Systems at Universitas Bina Sarana Informatika. I love diving into the ever-evolving world of technology, constantly exploring new concepts, tools, and methodologies to enhance my skills. For me, programming isn't just about writing code—it's about solving problems, creating impactful solutions, and continuously learning in this dynamic field.</p>
+              </div>
+              <div className="w-2/5 p-4">
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <p className="font-semibold">Location</p>
+                    <div className="flex items-center gap-2">
+                      <Image src={IndonesiaFlag} alt="Indonesia flag" className="w-6" />
+                      Purwokerto, ID
+                    </div>
+                  </div>
+                  <div>
+                    <p className="font-semibold">Website</p>
+                    <a className="underline flex items-center gap-4" href="https://pausberbuluu.blogspot.com/">pausberbuluu.blogspot.com <FiExternalLink /></a>
+                  </div>
+                  <div>
+                    <p className="font-semibold">Portfolio</p>
+                    <a className="underline flex items-center gap-4" href="https://ussporto.vercel.app/">ussporto.vercel.app <FiExternalLink /></a>
+                  </div>
+                  <div>
+                    <p className="font-semibold">Email</p>
+                    <a className="underline flex items-center gap-4" href="mailto:rizqybs.sp@gmail.com">rizqybs.sp@gmail.com <FiExternalLink /></a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold w-full lg:w-1/2 leading-tight"><span className="text-emerald-600">Enjoying Explore:</span> A Journey Through IT & Programming</h1>
-          <p className="py-6 text-base sm:text-lg lg:text-2xl w-full lg:w-2/3">
-            I enjoy exploring the vast world of IT, especially in programming. From learning various programming languages and mastering server management to experimenting with creative design, I&apos;m always eager to dive deeper into technology.
-            </p>
-            <button className="btn bg-emerald-600 text-white">Start Explore</button>
-          </div>
-      </div>
-      <div className="divider mb-12">Project</div>
-      <div className="tabs tabs-lift">
-        <label className="tab">
-          <input type="radio" name="project_tab" defaultChecked />
-          <FaLaravel className="mr-4" />
-          Laravel
-        </label>
-        <div className="tab-content bg-base-100 border-base-300 p-6">
-          {laravel.map((project, index) => (
-            <ProjectCard key={index} project={project} />
-          ))}
-        </div>
-        <label className="tab">
-          <input type="radio" name="project_tab" />
-          <FaUnity className="mr-4" />
-          Unity
-        </label>
-        <div className="tab-content bg-base-100 border-base-300 p-6">
-          {unity.map((project, index) => (
-            <ProjectCard key={index} project={project} />
-          ))}
-        </div>
-        <label className="tab">
-          <input type="radio" name="project_tab" />
-          <FaHtml5 className="mr-4" />
-          Html
-        </label>
-        <div className="tab-content bg-base-100 border-base-300 p-6">
-          {html.map((project, index) => (
-            <ProjectCard key={index} project={project} />
-          ))}
         </div>
       </div>
-      <button className="btn bg-emerald-600 block mx-auto my-10 text-white"><Link href="/project">See More Project...</Link></button>
-      <div className="divider mb-12">About</div>
-      <div>
-        <Image src={ussImg} alt="self-photo" className="w-80 max-[426px]:w-64 rounded-full block mx-auto mb-8"/>
-        <p className="text-center text-lg">&quot;Hi, I&apos;m Rizqy Bagus Saputra, a tech enthusiast with a deep passion for programming and innovation. I graduated from SMK N 1 Purwokerto, majoring in Software Engineering, and I&apos;m currently pursuing a degree in Information Systems at Universitas Bina Sarana Informatika. I love diving into the ever-evolving world of technology, constantly exploring new concepts, tools, and methodologies to enhance my skills. For me, programming isn&apos;t just about writing code—it&apos;s about solving problems, creating impactful solutions, and continuously learning in this dynamic field.&quot;</p>
-      </div>
-    </main>
-    <Footer />
     </>
 
   );
